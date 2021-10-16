a9e7e1ad17d7c6306331501572c256d4065008ad
helenavocado <hwang92@uchicago.edu>
2021-10-16 02:24:56 +0000

    safds

--- loops_fizz_buzz.rb
+++ loops_fizz_buzz.rb
@@ -6,11 +6,11 @@
 #   for numbers which are multiples of both three and five print "FizzBuzz"
 1.upto(100) do |x|
   if x%15==0
-    p "FizzBuzz"
+    p 'FizzBuzz'
   elsif x%3 ==0
-    p "Fizz"
+    p 'Fizz'
   elsif x%5==0
-    p "Buzz"
-  else p x.to_s
+    p 'Buzz'
+  else p x
   end
 end
